9a5eef52b04a0b8a9e09ee062999129a8f5bfd90
Tomer <tomer.kamar@gmail.com>
2024-02-07 19:36:57 +0200

    Add logic to weather service

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  private apiKey = '';
+  private apiUrl = 'https://dataservice.accuweather.com';
+
+  constructor(private http: HttpClient) { }
+
+  getLocation(query: string): Observable<any> {
+    const url = `${this.apiUrl}/locations/v1/cities/autocomplete`;
+    const params = { apiKey: this.apiKey, q: query };
+    return this.http.get(url, { params });
+  }
+
+  getCurrentWeather(locationKey: string): Observable<any> {
+    const url = `${this.apiUrl}/currentconditions/v1/${locationKey}`;
+    const params = { apiKey: this.apiKey };
+    return this.http.get(url, { params });
+  }
+
+  getLocationByLatLon(lat: number, lon: number): Observable<any> {
+    const url = `${this.apiUrl}/locations/v1/cities/geoposition/search`;
+    const params = { apiKey: this.apiKey, q: `${lat},${lon}` };
+    return this.http.get(url, { params });
+  }
+
+  get5Day(locationKey: string): Observable<any> {
+    const url = `${this.apiUrl}/forecasts/v1/daily/5day/${locationKey}`;
+    const params = { apiKey: this.apiKey };
+    return this.http.get(url, { params });
+  }
+
 }
+
