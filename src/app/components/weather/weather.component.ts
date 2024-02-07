f132fe03949a15cb72864f5e77e6af7717330173
Tomer <tomer.kamar@gmail.com>
2024-02-07 19:50:40 +0200

    Add logic to weather component

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WeatherService } from 'src/app/services/weather.service';
 
 @Component({
   selector: 'app-weather',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeatherComponent implements OnInit {
 
-  constructor() { }
+  searchCity: string = '';
+  weatherData: any;
+  isFavorite: boolean = false;
+
+  constructor(private weatherService: WeatherService) { }
 
   ngOnInit(): void {
+    this.setDefaultLocation();
+  }
+
+  searchWeather() {
+    this.weatherService.getLocation(this.searchCity).subscribe((locations: any) => {
+      if (locations && locations.lenght > 0) {
+        const locationKey = locations[0].key;
+        this.weatherService.getCurrentWeather(locationKey).subscribe((currentWeather: any) => {
+          this.weatherData = currentWeather;
+        });
+      }
+    });
+  }
+
+  toggleFavorite() {
+
+  }
+
+
+  private setDefaultLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        this.weatherService.getLocationByLatLon(lat, lon).subscribe((locationKey: any) => {
+          this.weatherService.getCurrentWeather(locationKey.key).subscribe((currentWeather: any) => {
+            this.weatherData = currentWeather;
+          });
+        });
+      });
+    }
+
   }
 
 }
